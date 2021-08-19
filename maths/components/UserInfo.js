c661fca97e79cadb7b28c79d97f2957d7f1c6695
narunbabu <narun.iitb@gmail.com>
2021-08-19 19:42:58 +0530

    userinfo

--- maths/components/UserInfo.js
+++ maths/components/UserInfo.js
@@ -1,3 +1,3 @@
-users={'parent':{'name':'','dob':'','dor':''}},'children':[{'id':0,'name':'','dob':'','class':'','dor':''}]
-todo=[{'childid':0,'arith_type':'','level':0,'n_of_probs':20,'set_date':0,'target_date':0}]
-score=[{'childid':0,'arith_type':'','level':0,'start_time':0,'end_time':0,'passed':false}]
+const users={'parent':{'name':'','dob':'','dor':''},'children':[{'id':0,'name':'','dob':'','class':'','dor':''}]}
+const todo=[{'childid':0,'arith_type':'','level':0,'n_of_probs':20,'set_date':0,'target_date':0}]
+const score=[{'childid':0,'arith_type':'','level':0,'start_time':0,'end_time':0,'passed':false}]
